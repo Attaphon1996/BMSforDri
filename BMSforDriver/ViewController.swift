25ec40ca8afd1a61e7a0f484d43bc4c68e801a33
อรรถพล วงษ์วิกิจการ <boatbright00@gmail.com>
2017-04-19 21:14:53 +0700

    Initial Commit

---
+++ BMSforDriver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMSforDriver
+//
+//  Created by อรรถพล วงษ์วิกิจการ on 4/19/2560 BE.
+//  Copyright © 2560 อรรถพล วงษ์วิกิจการ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
